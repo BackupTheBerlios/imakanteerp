a2ad783e606715aea6855d6cb7ffb91071465c97
imakante <imakante>
2005-06-02 11:36:00 +0000

    New file

---
+++ src/imakante/com/comunication.java
@@ -0,0 +1,28 @@
+/*
+ * comunication.java
+ *
+ * Created on Четвъвтък, 2005, Юни 2, 14:33
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package imakante.com;
+
+/**
+ *
+ * @author user
+ */
+public class comunication {
+    
+    /** Creates a new instance of comunication */
+    public comunication() {
+        
+        
+        
+        
+        
+    }
+    
+}
