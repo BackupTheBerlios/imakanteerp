99eedfd491187ea8b19047d8eb1f254dc0b58646
imakante <imakante>
2005-06-02 11:40:44 +0000

    New

--- src/imakante/com/comunication.java
+++ src/imakante/com/comunication.java
@@ -17,12 +17,38 @@ package imakante.com;
 public class comunication {
     
     /** Creates a new instance of comunication */
-    public comunication() {
+    
         
+     public static String setDBDriver(String strDriver){
+        String dbDriver = strDriver;
+        return dbDriver;
+    }
+    
+    
+    public String setDBSource(String strSource){
+        String dbURL = strSource;
+        return dbURL;
+    }
+    
+    
+    public String setUser(String strUser){
+        String dbUser = strUser;
+        return dbUser;
+    }
+    public String setFirm(String strFirm){
         
+        String dbName = strFirm;
+        return dbName;
+    }
+    
+    public String setPass(String strPass){
+        String dbPass = strPass;
+        return dbPass;
+    }
+    
         
         
         
     }
     
-}
+
