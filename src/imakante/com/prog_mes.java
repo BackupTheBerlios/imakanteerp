b4d2919d0ca7c3688d7b1eab1f20b3b1af6d756d
imakante <imakante>
2005-07-14 11:17:39 +0000

    up

--- src/imakante/com/prog_mes.java
+++ src/imakante/com/prog_mes.java
@@ -3,20 +3,30 @@ package imakante.com;
 
 import javax.swing.JOptionPane;
 
-public class prog_mes {
+public class prog_mes extends javax.swing.JOptionPane {
     
-    public static javax.swing.JOptionPane err_mes(int code){
-        String string_mess;
+    public prog_mes(int i){
+        String str_mes;
+        int newType = i;
+    switch(i){
+        case 1:str_mes = "Niama problemi"  ;break;
+        default : str_mes = "ima problemi";break;
+      
     
-        switch(code){
-        
-        case     1:string_mess = "NIama" ; break;
-        default   :string_mess = "problem"; break;
-        
-    }
-     javax.swing.JOptionPane jERR = new javax.swing.JOptionPane(string_mess,1);
-    return jERR;
     }
     
+    
+    }
+
+    public void setMessage(Object newMessage) {
+
+        super.setMessage(newMessage);
+    }
+
+    public void setMessageType(int newType) {
+
+        super.setMessageType(newType);
+    }
+  
     
 }
