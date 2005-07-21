52779521def44d563f3fd7fe72a5c6815171936a
imakante <imakante>
2005-07-21 06:56:37 +0000

    new

---
+++ src/imakante/salary/FrmModifikatori.java
@@ -0,0 +1,59 @@
+
+
+package imakante.salary;
+
+public class FrmModifikatori extends javax.swing.JInternalFrame {
+
+    public FrmModifikatori() {
+        initComponents();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jPanel2 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+
+        setTitle("\u041c\u043e\u0434\u0438\u0444\u0438\u043a\u0430\u0442\u043e\u0440\u0438 (\u041d\u0430\u0447\u0438\u0441\u043b\u0435\u043d\u0438\u044f \u0438 \u0423\u0434\u0440\u044a\u0436\u043a\u0438)");
+        jPanel1.setLayout(new java.awt.BorderLayout());
+
+        jPanel1.setBorder(new javax.swing.border.EtchedBorder());
+        jPanel1.add(jScrollPane1, java.awt.BorderLayout.CENTER);
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
+
+        jPanel2.setBorder(new javax.swing.border.EtchedBorder());
+        jButton1.setText("\u041d\u043e\u0432 \u0437\u0430\u043f\u0438\u0441");
+        jPanel2.add(jButton1);
+
+        jButton2.setText("\u0420\u0435\u0434\u0430\u043a\u0446\u0438\u044f");
+        jPanel2.add(jButton2);
+
+        jButton3.setText("\u0418\u0437\u0442\u0440\u0438\u0432\u0430\u043d\u0435");
+        jPanel2.add(jButton3);
+
+        jButton4.setText("\u041e\u043f\u0440\u0435\u0441\u043d\u044f\u0432\u0430\u043d\u0435");
+        jPanel2.add(jButton4);
+
+        getContentPane().add(jPanel2, java.awt.BorderLayout.SOUTH);
+
+        pack();
+    }
+    // </editor-fold>//GEN-END:initComponents
+    
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    // End of variables declaration//GEN-END:variables
+    
+}
