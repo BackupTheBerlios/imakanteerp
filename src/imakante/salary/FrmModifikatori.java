03c121d98f1d28f72572af78a9a6c25e582411a4
imakante <imakante>
2005-07-21 13:27:06 +0000

    UP

--- src/imakante/salary/FrmModifikatori.java
+++ src/imakante/salary/FrmModifikatori.java
@@ -2,12 +2,16 @@
 
 package imakante.salary;
 
-public class FrmModifikatori extends javax.swing.JInternalFrame {
-
-    public FrmModifikatori() {
+public class FrmModifikatori extends javax.swing.JInternalFrame implements java.awt.event.WindowListener {
+    
+    public FrmModifikatori(java.sql.Connection srcCN, javax.swing.JFrame getParentFrame) {
+        JFParentFrame = getParentFrame;
+        
+        dbInternal = srcCN;
+        initResource();
         initComponents();
     }
-
+    
     // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
     private void initComponents() {
         jPanel1 = new javax.swing.JPanel();
@@ -28,15 +32,39 @@ public class FrmModifikatori extends javax.swing.JInternalFrame {
 
         jPanel2.setBorder(new javax.swing.border.EtchedBorder());
         jButton1.setText("\u041d\u043e\u0432 \u0437\u0430\u043f\u0438\u0441");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         jPanel2.add(jButton1);
 
         jButton2.setText("\u0420\u0435\u0434\u0430\u043a\u0446\u0438\u044f");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         jPanel2.add(jButton2);
 
         jButton3.setText("\u0418\u0437\u0442\u0440\u0438\u0432\u0430\u043d\u0435");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         jPanel2.add(jButton3);
 
         jButton4.setText("\u041e\u043f\u0440\u0435\u0441\u043d\u044f\u0432\u0430\u043d\u0435");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
         jPanel2.add(jButton4);
 
         getContentPane().add(jPanel2, java.awt.BorderLayout.SOUTH);
@@ -44,6 +72,22 @@ public class FrmModifikatori extends javax.swing.JInternalFrame {
         pack();
     }
     // </editor-fold>//GEN-END:initComponents
+    
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+// TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
+    
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+// TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+    
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+// TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+    
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+// TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
     
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -53,7 +97,114 @@ public class FrmModifikatori extends javax.swing.JInternalFrame {
     private javax.swing.JButton jButton4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JScrollPane jScrollPane1;
+    public static javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
+    javax.swing.JFrame JFParentFrame;
+    public static java.sql.Connection dbInternal;
+    public static java.sql.Statement stm;
+    public static java.sql.ResultSet rsCus;
+    public static String strSQL;
+    public static imakante.com.CustomTableModel model;
+    public static imakante.com.CustomTable jTable;
+    public static final String Names[] = {"\u0412\u044a\u0442\u0440\u0435\u0448\u0435\u043d \u043d\u043e\u043c\u0435\u0440",
+            "\u041d\u043e\u043c\u0435\u0440","\u0418\u043c\u0435 \u043d\u0430 \u043e\u0442\u0434\u0435\u043b\u0430"
+    };
+    protected void newRecord(){
+        
+        imakante.salary.frmAddOtdel frmDialog = new imakante.salary.frmAddOtdel(JFParentFrame, true, dbInternal, 0);
+        frmDialog.setVisible(true);
+        
+        
+    }
+    
+    
+    protected void editRecord(){
+        if (jTable.getValueAt(jTable.getSelectedRow(), jTable.getSelectedColumn()) != null) {
+            int row = (Integer) jTable.getValueAt(jTable.getSelectedRow(),0);
+            try{
+                imakante.salary.frmAddOtdel frmDialog = new imakante.salary.frmAddOtdel(JFParentFrame, true, dbInternal, row);
+                frmDialog.setVisible(true);
+                
+            } catch(Exception e){e.printStackTrace();}
+        }
+    }
+    
+    protected void deleteRecord(){
+        try{
+            if (jTable.getValueAt(jTable.getSelectedRow(), jTable.getSelectedColumn()) != null) {
+                int row = (Integer) jTable.getValueAt(jTable.getSelectedRow(),0);
+                System.out.println("row "+row);
+                stm = dbInternal.createStatement(java.sql.ResultSet.TYPE_SCROLL_SENSITIVE,
+                        java.sql.ResultSet.CONCUR_UPDATABLE);
+                stm.execute("DELETE FROM ls_otdeli WHERE id = "+ row );
+                repaintTable();
+            }
+        } catch(java.sql.SQLException sqle){sqle.printStackTrace();}
+        
+        
+    }
+    
+    
+    
+    public static void repaintTable(){
+        jScrollPane1.remove(jTable);
+        initResource();
+        jScrollPane1.getViewport().add(jTable, null);
+        jScrollPane1.repaint(); jScrollPane1.getViewport().add(jTable, Names);
+        jScrollPane1.repaint();
+    }
+    
+    
+    protected static void initResource(){
+        try{
+            stm = dbInternal.createStatement(java.sql.ResultSet.TYPE_SCROLL_SENSITIVE,
+                    java.sql.ResultSet.CONCUR_UPDATABLE);
+        } catch (java.sql.SQLException sqle){sqle.printStackTrace();
+        }
+        
+        
+        try{
+            rsCus = stm.executeQuery("SELECT * FROM ls_otdeli");} catch (java.sql.SQLException sqle){sqle.printStackTrace();}
+        
+        model = new imakante.com.CustomTableModel(dbInternal, rsCus, Names);
+        jTable = new imakante.com.CustomTable(model);
+        
+        
+    }
+    protected void closeResource(){
+        if(rsCus!=null){ try{  rsCus.close();
+        }catch(java.sql.SQLException sqle){}}
+        if(stm!=null){ try{ stm.close();
+        }catch(java.sql.SQLException sqle){}}
+        
+    }
+    
+    
+    protected void UnloadWindow(){
+        closeResource();
+        this.dispose();
+        
+    }
+    
+    
+    
+    
+    public void windowOpened(java.awt.event.WindowEvent e){
+    }
+    public void windowClosing(java.awt.event.WindowEvent e){
+        UnloadWindow();
+    }
+    public void windowClosed(java.awt.event.WindowEvent e){
+    }
+    public void windowIconified(java.awt.event.WindowEvent e){
+    }
+    public void windowDeiconified(java.awt.event.WindowEvent e){
+    }
+    public void windowActivated(java.awt.event.WindowEvent e){
+    }
+    public void windowDeactivated(java.awt.event.WindowEvent e){
+    }
+    
+    
     
 }
