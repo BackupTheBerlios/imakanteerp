1bc1c731222c4f1ed4cfd660c5d5febe016cdd62
imakante <imakante>
2005-08-08 05:41:58 +0000

    up

--- src/imakante/salary/FrmDate.java
+++ src/imakante/salary/FrmDate.java
@@ -69,7 +69,7 @@ public class FrmDate extends JDialog //implements ActionListener
                     try{
                         
                         stm = dbInternal.createStatement();
-                        rsCus = stm.executeQuery("SELECT DISTINCT pyear FROM ls_result WHERE pyear IS NOT NULL ORDER BY pyear");
+                        rsCus = stm.executeQuery("SELECT DISTINCT ls_result.pyear FROM ls_result WHERE ls_result.pyear IS NOT NULL ORDER BY ls_result.pyear");
                         
                         while(rsCus.next()){
                             
