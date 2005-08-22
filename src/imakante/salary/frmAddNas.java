d918a9a37ef904f24aa227f3603be97d0e8a2324
imakante <imakante>
2005-08-22 15:46:13 +0000

    up

--- src/imakante/salary/frmAddNas.java
+++ src/imakante/salary/frmAddNas.java
@@ -23,10 +23,10 @@ public class frmAddNas extends javax.swing.JDialog implements java.awt.event.Win
         java.awt.GridBagConstraints gridBagConstraints;
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jComboBox1 = new javax.swing.JComboBox();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
@@ -35,20 +35,6 @@ public class frmAddNas extends javax.swing.JDialog implements java.awt.event.Win
         jPanel1.setLayout(new java.awt.GridBagLayout());
 
         jPanel1.setBorder(new javax.swing.border.EtchedBorder());
-        jLabel1.setText(java.util.ResourceBundle.getBundle("bundle/salary/names_bg").getString("city"));
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.insets = new java.awt.Insets(20, 20, 5, 5);
-        jPanel1.add(jLabel1, gridBagConstraints);
-
-        jLabel2.setText(java.util.ResourceBundle.getBundle("bundle/salary/names_bg").getString("area"));
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.insets = new java.awt.Insets(5, 20, 20, 5);
-        jPanel1.add(jLabel2, gridBagConstraints);
-
         jTextField1.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 jTextField1KeyPressed(evt);
@@ -56,6 +42,8 @@ public class frmAddNas extends javax.swing.JDialog implements java.awt.event.Win
         });
 
         gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
         gridBagConstraints.ipadx = 200;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
         gridBagConstraints.insets = new java.awt.Insets(20, 5, 5, 20);
@@ -75,35 +63,29 @@ public class frmAddNas extends javax.swing.JDialog implements java.awt.event.Win
         gridBagConstraints.insets = new java.awt.Insets(5, 5, 20, 20);
         jPanel1.add(jComboBox1, gridBagConstraints);
 
+        jLabel1.setText("\u041d\u0430\u0441\u0435\u043b\u0435\u043d\u043e \u043c\u044f\u0441\u0442\u043e");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 5, 5);
+        jPanel1.add(jLabel1, gridBagConstraints);
+
+        jLabel2.setText("\u041e\u0431\u043b\u0430\u0441\u0442");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.insets = new java.awt.Insets(5, 20, 20, 5);
+        jPanel1.add(jLabel2, gridBagConstraints);
+
         getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
 
         jPanel2.setBorder(new javax.swing.border.EtchedBorder());
-        jButton1.setText(java.util.ResourceBundle.getBundle("bundle/salary/names_bg").getString("save"));
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        jButton1.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                jButton1KeyPressed(evt);
-            }
-        });
-
+        jButton1.setText("\u0417\u0430\u043f\u0430\u0437\u0438");
         jPanel2.add(jButton1);
 
-        jButton2.setText(java.util.ResourceBundle.getBundle("bundle/salary/names_bg").getString("close"));
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-        jButton2.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                jButton2KeyPressed(evt);
-            }
-        });
-
+        jButton2.setText("\u0417\u0430\u0442\u0432\u043e\u0440\u0438");
         jPanel2.add(jButton2);
 
         getContentPane().add(jPanel2, java.awt.BorderLayout.SOUTH);
@@ -119,23 +101,7 @@ public class frmAddNas extends javax.swing.JDialog implements java.awt.event.Win
     private void jTextField1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField1KeyPressed
 // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1KeyPressed
-    
-    private void jButton2KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jButton2KeyPressed
-// TODO add your handling code here:
-    }//GEN-LAST:event_jButton2KeyPressed
-    
-    private void jButton1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jButton1KeyPressed
-// TODO add your handling code here:
-    }//GEN-LAST:event_jButton1KeyPressed
-    
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-// TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
-    
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-// TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
-    
+                    
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
