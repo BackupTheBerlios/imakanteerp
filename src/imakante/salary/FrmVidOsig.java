9540df3d1246270f99777ef575cfb1c559e09408
imakante <imakante>
2005-08-23 17:05:05 +0000

    1

---
+++ src/imakante/salary/FrmVidOsig.java
@@ -0,0 +1,76 @@
+
+package imakante.salary;
+
+public class FrmVidOsig extends javax.swing.JDialog {
+    
+    public FrmVidOsig(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jPanel3 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        jPanel1.setLayout(new java.awt.BorderLayout());
+
+        jPanel2.setLayout(new java.awt.BorderLayout());
+
+        jPanel2.setBorder(new javax.swing.border.EtchedBorder());
+        jPanel2.add(jScrollPane1, java.awt.BorderLayout.CENTER);
+
+        jPanel1.add(jPanel2, java.awt.BorderLayout.CENTER);
+
+        jPanel3.setBorder(new javax.swing.border.EtchedBorder());
+        jButton1.setFont(new java.awt.Font("Tahoma", 0, 11));
+        jButton1.setText("\u041d\u043e\u0432");
+        jPanel3.add(jButton1);
+
+        jButton2.setFont(new java.awt.Font("Tahoma", 0, 11));
+        jButton2.setText("\u0420\u0435\u0434\u0430\u043a\u0446\u0438\u044f");
+        jPanel3.add(jButton2);
+
+        jButton3.setFont(new java.awt.Font("Tahoma", 0, 11));
+        jButton3.setText("\u0418\u0437\u0442\u0440\u0438\u0432\u0430\u043d\u0435");
+        jPanel3.add(jButton3);
+
+        jButton4.setFont(new java.awt.Font("Tahoma", 0, 11));
+        jButton4.setText("\u041e\u043f\u0440\u0435\u0441\u043d\u044f\u0432\u0430\u043d\u0435");
+        jPanel3.add(jButton4);
+
+        jButton5.setFont(new java.awt.Font("Tahoma", 0, 11));
+        jButton5.setText("\u0417\u0430\u0442\u0432\u0430\u0440\u044f\u043d\u0435");
+        jPanel3.add(jButton5);
+
+        jPanel1.add(jPanel3, java.awt.BorderLayout.SOUTH);
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
+
+        pack();
+    }
+    // </editor-fold>//GEN-END:initComponents
+    
+
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    // End of variables declaration//GEN-END:variables
+    
+}
---
+++ src/imakante/salary/frmAL_VOsig.java
@@ -0,0 +1,50 @@
+
+package imakante.salary;
+
+public class frmAL_VOsig extends javax.swing.JDialog {
+
+    public frmAL_VOsig(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        jPanel1.setLayout(new java.awt.BorderLayout());
+
+        jPanel2.setLayout(new java.awt.BorderLayout());
+
+        jPanel2.setBorder(new javax.swing.border.EtchedBorder());
+        jPanel2.add(jScrollPane1, java.awt.BorderLayout.CENTER);
+
+        jPanel1.add(jPanel2, java.awt.BorderLayout.CENTER);
+
+        jPanel3.setBorder(new javax.swing.border.EtchedBorder());
+        jLabel1.setText("Insert-\u0417\u0430 \u0432\u044a\u0432\u0435\u0436\u0434\u0430\u043d\u0435 \u043d\u0430 \u0438\u0437\u0431\u0440\u0430\u043d\u0438\u044f \u0432\u0438\u0434 \u043e\u0441\u0438\u0433\u0443\u0440\u0435\u043d;    Esc - \u0417\u0430 \u043e\u0442\u043a\u0430\u0437");
+        jPanel3.add(jLabel1);
+
+        jPanel1.add(jPanel3, java.awt.BorderLayout.SOUTH);
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
+
+        pack();
+    }
+    // </editor-fold>//GEN-END:initComponents
+
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    // End of variables declaration//GEN-END:variables
+    
+}
