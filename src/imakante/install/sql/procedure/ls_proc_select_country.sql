7daea29da5b49a981a6c80c725dee3504a2d6811
imakante <imakante>
2005-10-15 06:55:42 +0000

    new

---
+++ src/imakante/install/sql/procedure/ls_proc_select_country.sql
@@ -0,0 +1,9 @@
+﻿DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `mida`.`ls_procedure_select_country`$$
+CREATE PROCEDURE `ls_procedure_select_country`()
+BEGIN
+  SELECT n.id, n.code, n.name FROM n_country n;
+END$$
+
+DELIMITER ;
