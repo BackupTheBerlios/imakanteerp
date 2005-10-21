eb4d40223873f7c7ffa6510afb45944af310eb2b
imakante <imakante>
2005-10-21 13:17:48 +0000

    up

---
+++ src/imakante/com/vcomponents/iMenu.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package imakante.com.vcomponents;
+
+
+/**
+ * @author dragiiski
+ *
+ */
+public class iMenu extends javax_swing_JMenu_PersistenceDelegate {
+
+	/**
+	 * 
+	 */
+	public iMenu() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/imakante/com/vcomponents/javax_swing_JMenu_PersistenceDelegate.java
@@ -0,0 +1,10 @@
+package imakante.com.vcomponents;
+
+public class javax_swing_JMenu_PersistenceDelegate {
+
+	public javax_swing_JMenu_PersistenceDelegate() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
