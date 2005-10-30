07d03ebc572b1cd585a0ac5d03c639114cb93998
imakante <imakante>
2005-10-30 16:10:09 +0000

    up

--- src/nom/FrmCountry.java
+++ src/nom/FrmCountry.java
@@ -1,19 +1,21 @@
 
 package nom;
 
+import java.awt.event.KeyEvent;
+
 public class FrmCountry extends imakante.com.vcomponents.iInternalFrame {
     
     public FrmCountry(String title, imakante.com.vcomponents.iFrame frame) {
         super("Strani");
-       
+        
         this.setClosable(true);
         this.setMaximizable(true);
         
         try{
-        setConn(frame.getConn());}catch(Exception e){e.printStackTrace();}
-      
+            setConn(frame.getConn());}catch(Exception e){e.printStackTrace();}
+        
         try{
-        countriesT = new nom.countries(getConn(), "", "");}catch(Exception e){e.printStackTrace();}
+            countriesT = new nom.countries(getConn(), "", "");}catch(Exception e){e.printStackTrace();}
         initTable();
         initComponents();
         
@@ -34,6 +36,7 @@ public class FrmCountry extends imakante.com.vcomponents.iInternalFrame {
         jPanel3 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
 
         jPanel1.setLayout(new java.awt.BorderLayout());
@@ -47,15 +50,32 @@ public class FrmCountry extends imakante.com.vcomponents.iInternalFrame {
 
         jTextField1.setMinimumSize(new java.awt.Dimension(60, 19));
         jTextField1.setPreferredSize(new java.awt.Dimension(60, 19));
+        jTextField1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jTextField1KeyPressed(evt);
+            }
+        });
+
         jPanel4.add(jTextField1);
 
         jLabel2.setText("\u0418\u043c\u0435");
         jPanel4.add(jLabel2);
 
         jTextField2.setPreferredSize(new java.awt.Dimension(250, 19));
+        jTextField2.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jTextField2KeyPressed(evt);
+            }
+        });
+
         jPanel4.add(jTextField2);
 
         jButton4.setText("\u0422\u044a\u0440\u0441\u0438");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
         jButton4.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 jButton4KeyPressed(evt);
@@ -100,6 +120,20 @@ public class FrmCountry extends imakante.com.vcomponents.iInternalFrame {
 
         jPanel3.add(jButton2);
 
+        jButton5.setText("\u0412\u0441\u0438\u0447\u043a\u0438 \u0437\u0430\u043f\u0438\u0441\u0438");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+        jButton5.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jButton5KeyPressed(evt);
+            }
+        });
+
+        jPanel3.add(jButton5);
+
         jButton3.setText("\u0417\u0430\u0442\u0432\u0430\u0440\u044f\u043d\u0435");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -119,39 +153,73 @@ public class FrmCountry extends imakante.com.vcomponents.iInternalFrame {
         pack();
     }
     // </editor-fold>//GEN-END:initComponents
-
+    
+    private void jButton5KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jButton5KeyPressed
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER){ jButton5.doClick();}
+    }//GEN-LAST:event_jButton5KeyPressed
+    
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        jScrollPane1.remove(table);
+        model = new imakante.com.CustomTableModel(getConn(), countriesT.getRs("", ""), null);
+        table = new imakante.com.CustomTable(model);
+        jScrollPane1.getViewport().add(table);
+        jScrollPane1.repaint();
+        
+    }//GEN-LAST:event_jButton5ActionPerformed
+    
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        searchRecords();
+    }//GEN-LAST:event_jButton4ActionPerformed
+    
+    private void jTextField2KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField2KeyPressed
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER){ jTextField2.transferFocus();}
+    }//GEN-LAST:event_jTextField2KeyPressed
+    
+    private void jTextField1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField1KeyPressed
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER){ jTextField1.transferFocus();}
+    }//GEN-LAST:event_jTextField1KeyPressed
+    
     private void jButton4KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jButton4KeyPressed
-
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER){ jButton4.doClick();}
     }//GEN-LAST:event_jButton4KeyPressed
-
+    
     private void jButton3KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jButton3KeyPressed
-
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER){ jButton3.doClick();}
     }//GEN-LAST:event_jButton3KeyPressed
-
+    
     private void jButton2KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jButton2KeyPressed
-
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER){ jButton2.doClick();}
     }//GEN-LAST:event_jButton2KeyPressed
-
+    
     private void jButton1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jButton1KeyPressed
-// TODO add your handling code here:
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER){ jButton1.doClick();}
     }//GEN-LAST:event_jButton1KeyPressed
-
+    
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         
     }//GEN-LAST:event_jButton2ActionPerformed
-
+    
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         
     }//GEN-LAST:event_jButton1ActionPerformed
-
+    
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         
     }//GEN-LAST:event_jButton3ActionPerformed
+    private void searchRecords(){
+        jScrollPane1.remove(table);
+        try{
+            model = new imakante.com.CustomTableModel(getConn(), countriesT.getRs(jTextField1.getText(),jTextField2.getText()), null);
+            table = new imakante.com.CustomTable(model);
+        }catch(Exception e){e.printStackTrace();}
+        jScrollPane1.getViewport().add(table);
+        jScrollPane1.repaint();
+    }
     
     private void initTable() {
         try{
-        model = new imakante.com.CustomTableModel(getConn(), countriesT.getRs(), null);
-        table = new imakante.com.CustomTable(model);
+            model = new imakante.com.CustomTableModel(getConn(), countriesT.getRs(), null);
+            table = new imakante.com.CustomTable(model);
         }catch(Exception e){e.printStackTrace();}
     }
     
@@ -174,6 +242,7 @@ public class FrmCountry extends imakante.com.vcomponents.iInternalFrame {
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
