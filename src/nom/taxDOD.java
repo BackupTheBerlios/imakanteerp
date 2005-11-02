055e28d7d109f9fc3f4cea361241c406937d016d
imakante <imakante>
2005-11-02 13:24:53 +0000

    up

--- src/nom/taxDOD.java
+++ src/nom/taxDOD.java
@@ -7,15 +7,21 @@ public class taxDOD extends dbObject {
 
     public taxDOD(java.sql.Connection conn, int in_year) {
         super(conn);
-        setPeriod(in_year);
+        
         prepareCstm();
+        setPeriod(in_year);
     }
  
     private java.sql.Connection conn;
     private java.sql.CallableStatement cstm;
     private java.sql.ResultSet rs;
-    private int period;
-    private int mod; 
+    private int period = 0;
+    private int comprator = 0;
+    private int in_id = 0;
+    private String in_date = "0000-00-00";
+    private double in_doh = 0;
+    private double in_sum = 0;
+    private double in_prct=0;
             
     private double[] taxDOD_doh = new double[10];
     private double[] taxDOD_sum = new double[10];
@@ -24,8 +30,15 @@ public class taxDOD extends dbObject {
     
     public void registerParameters(){
         try{
-            cstm.setInt("mod", getMod());
-            cstm.setInt("period", getPeriod());
+            
+            cstm.setInt("comprator", getComprator());
+            cstm.setInt("in_year", getPeriod());
+            cstm.setInt("in_id", getIn_id());
+            cstm.setString("in_date", getIn_date());
+            cstm.setDouble("in_doh", getIn_doh());
+            cstm.setDouble("in_sum", getIn_sum());
+            cstm.setDouble("in_prct", getIn_prct());
+            
             
         }catch(java.sql.SQLException sqle){sqle.printStackTrace();}
     }
@@ -52,14 +65,7 @@ public class taxDOD extends dbObject {
     
     public void setPeriod(int period) {
         this.period = period;
-    }
-
-    public int getMod() {
-        return mod;
-    }
-
-    public void setMod(int mod) {
-        this.mod = mod;
+        
     }
 
     public double[] getTaxDOD_doh() {
@@ -84,6 +90,54 @@ public class taxDOD extends dbObject {
 
     public void setTaxDOD_prct(double[] taxDOD_prct) {
         this.taxDOD_prct = taxDOD_prct;
+    }
+
+    public int getComprator() {
+        return comprator;
+    }
+
+    public void setComprator(int comprator) {
+        this.comprator = comprator;
+    }
+
+    public int getIn_id() {
+        return in_id;
+    }
+
+    public void setIn_id(int in_id) {
+        this.in_id = in_id;
+    }
+
+    public String getIn_date() {
+        return in_date;
+    }
+
+    public void setIn_date(String in_date) {
+        this.in_date = in_date;
+    }
+
+    public double getIn_doh() {
+        return in_doh;
+    }
+
+    public void setIn_doh(double in_doh) {
+        this.in_doh = in_doh;
+    }
+
+    public double getIn_sum() {
+        return in_sum;
+    }
+
+    public void setIn_sum(double in_sum) {
+        this.in_sum = in_sum;
+    }
+
+    public double getIn_prct() {
+        return in_prct;
+    }
+
+    public void setIn_prct(double in_prct) {
+        this.in_prct = in_prct;
     }
     
     
