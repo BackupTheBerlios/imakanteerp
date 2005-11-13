68ffff65b4c8818e08feddc3a500d264731fa765
imakante <imakante>
2005-11-13 10:52:41 +0000

    n

---
+++ src/imakante/com/vcomponents/iDialog.java
@@ -0,0 +1,17 @@
+
+
+package imakante.com.vcomponents;
+
+public class iDialog extends javax.swing.JDialog implements java.io.Serializable {
+
+    public iDialog(imakante.com.vcomponents.iInternalFrame frame, boolean modal) {
+          super();
+          this.setModal(modal);
+          setMyFrame(frame);
+    }
+    private imakante.com.vcomponents.iInternalFrame myIFrame;
+    
+    private void setMyFrame(imakante.com.vcomponents.iInternalFrame myFrame) {
+        this.myIFrame = myFrame;
+    }
+}
---
+++ src/nom/aeCountry.java
@@ -0,0 +1,80 @@
+
+package nom;
+
+public class aeCountry extends imakante.com.vcomponents.iDialog{
+
+    public aeCountry(imakante.com.vcomponents.iInternalFrame frame, boolean ismodal) {
+        super(frame, ismodal);
+        initComponents();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jPanel2 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        jPanel1.setLayout(new java.awt.GridBagLayout());
+
+        jPanel1.setBorder(new javax.swing.border.TitledBorder(""));
+        jLabel1.setText("\u041d\u043e\u043c\u0435\u0440");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 5, 5);
+        jPanel1.add(jLabel1, gridBagConstraints);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.insets = new java.awt.Insets(20, 5, 5, 20);
+        jPanel1.add(jLabel2, gridBagConstraints);
+
+        jLabel3.setText("\u0414\u044a\u0440\u0436\u0430\u0432\u0430");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.insets = new java.awt.Insets(5, 20, 20, 5);
+        jPanel1.add(jLabel3, gridBagConstraints);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.ipadx = 200;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 20, 20);
+        jPanel1.add(jTextField1, gridBagConstraints);
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
+
+        jButton1.setText("\u0417\u0430\u043f\u0430\u0437\u0438");
+        jPanel2.add(jButton1);
+
+        jButton2.setText("\u0417\u0430\u0442\u0432\u043e\u0440\u0438");
+        jPanel2.add(jButton2);
+
+        getContentPane().add(jPanel2, java.awt.BorderLayout.SOUTH);
+
+        pack();
+    }
+    // </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+    
+}
