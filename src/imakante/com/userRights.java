5486f92cc6b171757dc7322bed1d3a23063fcab7
imakante <imakante>
2005-12-27 17:04:45 +0000

    up

--- src/imakante/com/userRights.java
+++ src/imakante/com/userRights.java
@@ -2,7 +2,7 @@
 package imakante.com;
 
 public class userRights {
- // Obekt sadarjash pravata na usera
+    // Obekt sadarjash pravata na usera
     
     
     public userRights(java.sql.Connection conn) {
@@ -18,11 +18,20 @@ public class userRights {
     
     // Method for extracting user rights
     public int getRight(int modul, int rightn){
-    int inmodul = modul;
-    int inright = rightn;
-    int outright = 0;
-    
-    return outright;
+        
+        int outright = 0;
+        if (modul == 0){
+            outright = Integer.valueOf((String) modul0.get(""+modul));
+        }else if(modul == 1){
+            outright = Integer.valueOf((String) modul1.get(""+modul));
+        }else if(modul == 2){
+            outright = Integer.valueOf((String) modul2.get(""+modul));
+        }else if(modul == 3){
+            outright = Integer.valueOf((String) modul3.get(""+modul));
+        }else if(modul == 4){
+            outright = Integer.valueOf((String) modul4.get(""+modul));
+        }
+        return outright;
     }
     
 }
