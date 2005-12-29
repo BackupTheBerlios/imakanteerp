acb76f6fbe3f6cf19fdbbf6c71533fab552c45d4
ikacarov <ikacarov>
2005-12-29 17:45:51 +0000

    no message

--- src/imakante/install/sql/procedure/ls_procedure_group.sql
+++ src/imakante/install/sql/procedure/ls_procedure_group.sql
@@ -1,43 +1,43 @@
 ﻿DELIMITER $$
 
-DROP PROCEDURE IF EXISTS `frmtest`.`ls_procedure_group` $$
-CREATE PROCEDURE `ls_procedure_group`(IN in_id INT(6), IN in_nom TINYINT, IN comprator TINYINT, IN in_cod VARCHAR(30), IN in_name VARCHAR(30), IN in_alid TINYINT)
+DROP PROCEDURE IF EXISTS `mida`.`ls_procedure_group` $$
+CREATE PROCEDURE `mida`.`ls_procedure_group`(IN in_id INT(6), IN in_nom TINYINT, IN comprator TINYINT, IN in_cod VARCHAR(30), IN in_name VARCHAR(30), IN in_alid TINYINT)
 BEGIN
      IF (comprator = 0) THEN
-        SELECT n.id, n.cod, n.name, n.alid FROM `frmtest`.`group` n WHERE nom = in_nom;
+        SELECT n.id, n.cod, n.name, n.alid FROM `mida`.`group` n WHERE nom = in_nom;
      END IF;
      IF (comprator = 1) THEN
-        INSERT INTO `frmtest`.`group`(nom, cod, name, alid) VALUES(in_nom, in_cod, in_name, in_alid);
+        INSERT INTO `mida`.`group`(nom, cod, name, alid) VALUES(in_nom, in_cod, in_name, in_alid);
      END IF;
      IF (comprator = 2) THEN
-        UPDATE `frmtest`.`group` SET cod = in_cod, name = in_name, nom = in_nom, alid = in_alid
-           WHERE `frmtest`.`group`.id = in_id;
+        UPDATE `mida`.`group` SET cod = in_cod, name = in_name, nom = in_nom, alid = in_alid
+           WHERE `mida`.`group`.id = in_id;
      END IF;
 
      IF (comprator = 3) THEN
-        DELETE FROM `frmtest`.`group`  WHERE id = in_id;
+        DELETE FROM `mida`.`group`  WHERE id = in_id;
      END IF;
 
      IF (comprator = 4) THEN
-        SELECT n.id, n.nom, n.cod, n.name, n.alid FROM `frmtest`.`group` n WHERE `frmtest`.`group`.id = in_id;
+        SELECT n.id, n.nom, n.cod, n.name, n.alid FROM `mida`.`group` n WHERE `mida`.`group`.id = in_id;
 
      END IF;
      IF (comprator = 5) THEN
-        SELECT n.id,  n.cod, n.name, n.alid  FROM `frmtest`.`group` n  WHERE n.cod LIKE in_cod AND
-        n.name LIKE in_name AND n.alid LIKE in_alid AND n.nom=in_nom;
+        SELECT n.id,  n.cod, n.name, n.alid  FROM `mida`.`group` n  WHERE n.cod LIKE CONCAT('%',in_cod,'%') AND
+        n.name LIKE CONCAT('%',in_name,'%') AND n.alid LIKE CONCAT(in_alid,'%') AND n.nom=in_nom;
      END IF;
      IF (comprator = 6) THEN
-        SELECT * FROM `frmtest`.`anlevel` n;
+        SELECT * FROM `mida`.`anlevel` n ORDER BY cod;
      END IF;
 
 
 
      IF (comprator = 7) THEN
-        SELECT MAX(n.id) AS id FROM `frmtest`.`group` n;
+        SELECT MAX(n.id) AS id FROM `mida`.`group` n;
      END IF;
 
      IF (comprator = 8) THEN
-       DELETE FROM `frmtest`.`group`;
+       DELETE FROM `mida`.`group`;
      END IF;
 END $$
 
