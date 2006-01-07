b5b57e44fecf5a7048b391391d2d61ca75b214df
imakante <imakante>
2006-01-07 09:51:08 +0000

    up

---
+++ src/imakante/install/sql/sl_n_casa.sql
@@ -0,0 +1,9 @@
+﻿DROP TABLE IF EXISTS `mida`.`sl_n_casa`;
+CREATE TABLE  `mida`.`sl_n_casa` (
+  `idsl_casa` int(10) unsigned NOT NULL auto_increment,
+  `gr_casa` int(10) unsigned NOT NULL default '0',
+  `cod_casa` int(10) unsigned NOT NULL default '1',
+  `name_casa` varchar(45) NOT NULL default 'каса',
+  `comments` varchar(250) default NULL,
+  PRIMARY KEY  (`idsl_casa`)
+) ENGINE=InnoDB DEFAULT CHARSET=cp1251;
