d4b76a7d88b6e86e78c43311e683b855b0c90e99
ikacarov <ikacarov>
2006-01-16 22:16:08 +0000

    no message

---
+++ src/imakante/install/sql/procedure/nom_procedure_product.sql
@@ -0,0 +1,46 @@
+﻿DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `mida`.`nom_procedure_product` $$
+CREATE PROCEDURE  `mida`.`nom_procedure_product`(IN in_comprator TINYINT,    IN in_id_pm INT(11),              IN in_id_ppp INT(11),        IN in_id_pp INT(11),
+                                                 IN in_id_pf INT(11),        IN in_id_n_group INT(11),         IN in_id_pd INT(11),         IN in_name_pm VARCHAR(30),
+                                                 IN in_sname_pm VARCHAR(20), IN in_fname_pm VARCHAR(20),       IN in_cname_pm VARCHAR(20),  IN in_max_pop_pm INT(3),
+                                                 IN in_flag_pm TINYINT,      IN in_expertsheet_pm VARCHAR(45), IN in_barcod_pm INT,         IN in_cod1_pm VARCHAR(20),
+                                                 IN in_cod2_pm VARCHAR(20))
+
+BEGIN
+     IF (in_comprator = 0) THEN
+        SELECT n.id_pm, n.id_n_group, n.id_ppp, n.id_pp, n.id_pf, n.name_pm, n.fname_pm, n.sname_pm, n.cname_pm, n.cod1_pm,
+         n.cod2_pm, n.barcod_pm, n.max_pop_pm, n.expertsheet_pm, n.flag_pm, pp.id_n_money, pp.id_sl_curs, pp.price_pp, pf.dds_pf, pf.excise_pf,
+         pf.other_pf, pd.id_pm, pd.m1_pd, pd.v1_pd, pd.m2_pd, pd.v2_pd, pd.m3_pd, pd.v3_pd, n.id_pd
+        FROM n_product_main n LEFT JOIN n_product_price pp ON pp.id_pp = n.id_pp LEFT JOIN n_product_fee pf ON pf.id_pf = n.id_pf LEFT JOIN
+         n_product_description pd ON pd.id_pd = n.id_pd LEFT JOIN n_product_promotion_price ppp ON ppp.id_ppp = n.id_ppp LEFT JOIN n_group g
+         ON g.id_n_group = n.id_n_group;
+     END IF;
+     IF (in_comprator = 1) THEN
+        INSERT INTO n_product_main(id_n_group,id_ppp,id_pp, id_pf,name_pm, fname_pm, sname_pm,cname_pm,cod1_pm,
+          cod2_pm,  barcod_pm,  max_pop_pm,  expertsheet_pm,  flag_pm, id_pd)  VALUES(in_id_n_group,in_id_ppp,in_id_pp, in_id_pf,in_name_pm,
+          in_fname_pm, in_sname_pm, in_cname_pm, in_cod1_pm, in_cod2_pm,  in_barcod_pm, in_max_pop_pm, in_expertsheet_pm, in_flag_pm, in_id_pd);
+     END IF;
+     IF (in_comprator = 2) THEN
+       UPDATE `mida`.`n_product_main` SET flag_pm = in_flag_pm
+        WHERE `mida`.`n_nm`.id = in_id;
+     END IF;
+
+
+     IF (in_comprator = 4) THEN
+         SELECT n.id_n_group, n.id_ppp, n.id_pp, n.id_pf, n.name_pm, n.fname_pm, n.sname_pm, n.cname_pm, n.cod1_pm, n.cod2_pm, n.barcod_pm, n.max_pop_pm, n.expertsheet_pm, n.flag_pm, n.id_pd
+          FROM n_product_main n WHERE n.id_pm = in_id_pm;
+     END IF;
+
+
+
+
+
+
+     IF (in_comprator = 7) THEN
+        SELECT MAX(n.id_pm) AS id FROM `mida`.`n_product_main` n;
+     END IF;
+
+END $$
+
+DELIMITER ;
