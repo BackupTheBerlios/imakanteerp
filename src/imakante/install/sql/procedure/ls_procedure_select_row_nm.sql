24fa89e4b9aa905c99b9e705dbc5167a9c7daad6
imakante <imakante>
2006-01-29 12:55:56 +0000

    up

--- src/imakante/install/sql/procedure/ls_procedure_select_row_nm.sql
+++
@@ -1,8 +0,0 @@
-﻿DELIMITER $$
-
-CREATE PROCEDURE `mida`.`ls_procedure_select_row_nm` (row_index INT)
-BEGIN
-      SELECT n.id, n.postcode, n.name, n.id_oblast FROM n_nm n WHERE n.id = row_index;
-END$$
-
-DELIMITER ;
