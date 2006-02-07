d1e47c9e33c865357e5f59e8c911034f0ae476b3
johnniepop <johnniepop>
2006-02-07 12:33:28 +0000

    äîáàâåíî ïîëå 'code_ls_n_person'

--- src/imakante/install/sql/procedure/nom_procedure_person.sql
+++ src/imakante/install/sql/procedure/nom_procedure_person.sql
@@ -1,19 +1,19 @@
 ﻿DELIMITER $$
 
 DROP PROCEDURE IF EXISTS `mida`.`nom_procedure_person` $$
-CREATE PROCEDURE `nom_procedure_person`(IN comprator TINYINT, IN in_id INT(11),IN in_id_group INT(11), IN in_egn VARCHAR(10), IN in_nomlk VARCHAR(9), IN in_name VARCHAR(45), IN in_comment VARCHAR(250) )
+CREATE PROCEDURE `nom_procedure_person`(IN comprator TINYINT, IN in_id INT(11),IN in_id_group INT(11), IN in_code VARCHAR(6), IN in_egn VARCHAR(10), IN in_nomlk VARCHAR(9), IN in_name VARCHAR(45), IN in_comment VARCHAR(250) )
 BEGIN
      IF (comprator = 0) THEN
-          SELECT n.id_ls_n_person, n.id_n_group, ng.name_n_group, n.egn_ls_n_person, n.nlk_ls_n_person, n.name_ls_n_person,
+          SELECT n.id_ls_n_person, n.id_n_group, ng.name_n_group, n.code_ls_n_person, n.egn_ls_n_person, n.nlk_ls_n_person, n.name_ls_n_person,
                    n.comment_ls_n_person FROM ls_n_person n LEFT OUTER JOIN n_group ng ON ng.id_n_group = n.id_n_group;
      END IF;
 
      IF (comprator = 1) THEN
-        INSERT INTO `mida`.`ls_n_person`(id_n_group, egn_ls_n_person, nlk_ls_n_person, name_ls_n_person, comment_ls_n_person) VALUES(in_id_group, in_egn, in_nomlk, in_name, in_comment);
+        INSERT INTO `mida`.`ls_n_person`(id_n_group, code_ls_n_person, egn_ls_n_person, nlk_ls_n_person, name_ls_n_person, comment_ls_n_person) VALUES(in_id_group, in_code, in_egn, in_nomlk, in_name, in_comment);
      END IF;
 
      IF (comprator = 2) THEN
-        UPDATE `mida`.`ls_n_person` SET id_n_group = in_id_group, egn_ls_n_person = in_egn, nlk_ls_n_person = in_nomlk, name_ls_n_person = in_name, comment_ls_n_person = in_comment
+        UPDATE `mida`.`ls_n_person` SET id_n_group = in_id_group, code_ls_n_person = in_code, egn_ls_n_person = in_egn, nlk_ls_n_person = in_nomlk, name_ls_n_person = in_name, comment_ls_n_person = in_comment
         WHERE `mida`.`ls_n_person`.id_ls_n_person = in_id;
      END IF;
 
@@ -22,9 +22,9 @@ BEGIN
      END IF;
 
      IF (comprator = 5) THEN
-        SELECT n.id_ls_n_person, n.id_n_group, ng.name_n_group, n.egn_ls_n_person, n.nlk_ls_n_person, n.name_ls_n_person,
+        SELECT n.id_ls_n_person, n.id_n_group, ng.name_n_group, n.code_ls_n_person, n.egn_ls_n_person, n.nlk_ls_n_person, n.name_ls_n_person,
                    n.comment_ls_n_person FROM ls_n_person n LEFT OUTER JOIN n_group ng ON ng.id_n_group = n.id_n_group
-                   WHERE n.egn_ls_n_person LIKE CONCAT('%',in_egn,'%') AND n.name_ls_n_person LIKE CONCAT('%',in_name,'%');
+                   WHERE n.code_ls_n_person LIKE CONCAT('%',in_code,'%') AND n.egn_ls_n_person LIKE CONCAT('%',in_egn,'%') AND n.name_ls_n_person LIKE CONCAT('%',in_name,'%');
      END IF;
 
      IF (comprator = 6) THEN
@@ -34,6 +34,10 @@ BEGIN
 
      IF (comprator = 7) THEN
         SELECT MAX(n.id_ls_n_person) FROM `mida`.`ls_n_person` n;
+     END IF;
+
+     IF (comprator = 8) THEN
+        SELECT MAX(n.code_ls_n_person) AS code_ls_n_person FROM `mida`.`ls_n_person` n;
      END IF;
 
      IF (comprator = 9) THEN
