0b8cf8217f7aed8c5874ba406296a927ef1f0ad6
johnniepop <johnniepop>
2006-02-10 18:37:27 +0000

    new

---
+++ src/imakante/install/sql/procedure/sl_procedure_num_doc.sql
@@ -0,0 +1,42 @@
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `mida`.`sl_doc_type_num` $$
+CREATE PROCEDURE `sl_procedure_num_doc`(IN comprator TINYINT, IN in_id INT(11),IN in_id_groupe INT(11), IN in_code INT(11), IN in_name VARCHAR(40), IN in_comments VARCHAR(250) )
+BEGIN
+     IF (comprator = 0) THEN
+          SELECT n.id_sdtn, n.id_ntd, ng.name_n_group, n.code_n_casa, n.name_n_casa,
+                   n.comments_n_casa FROM n_casa n LEFT OUTER JOIN n_group ng ON ng.id_n_group=n.id_n_group;
+     END IF;
+     IF (comprator = 1) THEN
+        INSERT INTO `mida`.`n_casa`( id_n_group, code_n_casa, name_n_casa, comments_n_casa) VALUES(in_id_groupe, in_code, in_name, in_comments);
+     END IF;
+     IF (comprator = 2) THEN
+        UPDATE `mida`.`n_casa` SET id_n_group = in_id_groupe, code_n_casa = in_code,   name_n_casa = in_name, comments_n_casa = in_comments
+        WHERE `mida`.`n_casa`.id_n_casa = in_id;
+     END IF;
+     IF (comprator = 3) THEN
+        DELETE FROM `mida`.`n_casa`  WHERE id_n_casa = in_id;
+     END IF;
+
+     IF (comprator = 5) THEN
+        SELECT n.id_n_casa, n.id_n_group, ng.name_n_group, n.code_n_casa, n.name_n_casa,
+                   n.comments_n_casa FROM n_casa n LEFT OUTER JOIN n_group ng ON ng.id_n_group=n.id_n_group
+                   WHERE n.code_n_casa LIKE CONCAT('%',in_code,'%') AND  n.code_n_casa LIKE CONCAT('%',in_name,'%');
+     END IF;
+     IF (comprator = 6) THEN
+        SELECT n.id_n_group, n.name_n_group FROM n_group n
+                              WHERE n.nom_n_group = 5;
+     END IF;
+     IF (comprator = 7) THEN
+        SELECT MAX(n.id_n_casa) FROM `mida`.`n_casa` n;
+     END IF;
+     IF (comprator = 8) THEN
+        SELECT MAX(n.code_n_casa) AS code_n_casa FROM `mida`.`n_casa` n;
+     END IF;
+     IF (comprator = 9) THEN
+        SELECT MAX(n.id_n_group) AS id_n_group FROM n_group n
+                              WHERE n.nom_n_group = 5;
+     END IF;
+END $$
+
+DELIMITER ;
---
+++ src/imakante/sales/aeNumDoc.java
@@ -0,0 +1,32 @@
+
+package imakante.sales;
+
+public class aeNumDoc extends javax.swing.JDialog {
+    
+    public aeNumDoc(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                .add(0, 400, Short.MAX_VALUE)
+                );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                .add(0, 300, Short.MAX_VALUE)
+                );
+        pack();
+    }
+    // </editor-fold>//GEN-END:initComponents
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+    
+}
