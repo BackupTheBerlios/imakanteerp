46cbfe387419e414e0cbb1718a97641d345dce75
imakante <imakante>
2006-02-13 15:06:28 +0000

    up

--- src/nom/casaDB.java
+++ src/nom/casaDB.java
@@ -31,29 +31,15 @@ public class casaDB  extends dbObject {
         prepareCstm();
         
     }
-    //-------SART MyFunction
-    private void prepareCstm() // OK
+      protected void prepareCstm()
     {
         try {
             
             setCstm(getConn().prepareCall("{call nom_procedure_casa(?,?,?,?,?,?)}"));
             
-        } catch(java.sql.SQLException sqle) {sqle.printStackTrace();}
+        } catch (SQLException sqle) {sqle.printStackTrace();}
     }
-    public java.sql.ResultSet getTable() //OK
-    {
-        
-        this.comprator = 0;
-        try{
-            registerParameters();
-            setRs(getCstm().executeQuery());
-        } catch(java.sql.SQLException sqle) {
-            sqle.printStackTrace();
-        }
-        
-        return rs;
-    }
-    private void registerParameters() //OK
+    protected void registerParameters() //OK
     {
         try {
             getCstm().setInt("comprator", getComprator());
@@ -64,29 +50,6 @@ public class casaDB  extends dbObject {
             getCstm().setString("in_comments", getComment());
                        
             
-        } catch(java.sql.SQLException sqle) {
-            sqle.printStackTrace();
-        }
-        
-    }
-    private void prepareRezult() //OK
-    {
-        try{
-            registerParameters();
-            setRs(getCstm().executeQuery());}catch(java.sql.SQLException sqle){sqle.printStackTrace();}
-        
-    }
-    public void insertRow(int in_code, int in_id_groupe) // OK  comprator = 1;
-    {
-        comprator = 1;
-        
-        this.code = in_code;
-        this.name = "";
-        this.id_groupe = in_id_groupe;
-        this.comment = "";
-        try {
-            registerParameters();
-            cstm.execute();
         } catch(java.sql.SQLException sqle) {
             sqle.printStackTrace();
         }
@@ -109,35 +72,6 @@ public class casaDB  extends dbObject {
         
         
     }
-    public void deleteRow(int in_id) //OK   comprator = 3;
-    {
-        comprator = 3;
-        id = in_id;
-        try{
-            registerParameters();
-            cstm.execute();
-        }catch(java.sql.SQLException sqle){sqle.printStackTrace();}
-        
-    }
-    
-    public java.sql.ResultSet getRow(int in_id) //OK  comprator = 4;
-    {
-        comprator = 4;
-        id = in_id;
-        try {
-            registerParameters();
-            rs = cstm.executeQuery();
-            while(rs.next()) {
-                id_groupe = rs.getInt("id_groupe");
-                name = rs.getString("name");
-                code = rs.getInt("code");
-                comment = rs.getString("comments");
-            }
-        } catch(java.sql.SQLException sqle) {
-            sqle.printStackTrace();
-        }
-        return rs;
-    }
     
     public java.sql.ResultSet searchRecords( int in_code, String in_name) // -OK  comprator = 5;
     {
@@ -155,90 +89,7 @@ public class casaDB  extends dbObject {
         return getRs();
         
     }
-   
-    
-    public java.sql.Connection getConn() //OK
-    {
-        return conn;
-    }
-    
-    public void setConn(java.sql.Connection conn) //OK
-    {
-        this.conn = conn;
-    }
-    
-    public java.sql.Statement getStm() //OK
-    {
-        return stmt;
-    }
-    
-    public void setStm(java.sql.Statement stm) //KO
-    {
-        this.stmt = stm;
-    }
-    
-    public java.sql.CallableStatement getCstm() //OK
-    {
-        return cstm;
-    }
-    
-    public void setCstm(java.sql.CallableStatement cstm) //OK
-    {
-        this.cstm = cstm;
-    }
-    
-    public java.sql.ResultSet getRs() //OK
-    {
-        return rs;
-    }
-    public void setRs(java.sql.ResultSet rs) //OK
-    {
-        this.rs = rs;
-    }
-    public void setId(int ID) //OK
-    {
-        this.id = ID;
-    }
-    public int getId() //OK
-    {
-        return id;
-    }
-    
-    
-    public int getMaxId() //OK    comprator = 7;
-    {
-        comprator = 7;
-        int return_int=-1;
-        try {
-            registerParameters();
-            rs = cstm.executeQuery();
-            while(rs.next()) {
-                return_int = rs.getInt(1);
-            }
-        } catch(java.sql.SQLException sqle) {
-            sqle.printStackTrace();
-        }
-        return return_int;
-    }
-    
-    
-    
-    public int getMaxCod() //OK    comprator = 8;
-    {
-        comprator = 8;
-        int return_int=-1;
-        try {
-            registerParameters();
-            rs = cstm.executeQuery();
-            while(rs.next()) {
-                return_int = rs.getInt(1);
-            }
-        } catch(java.sql.SQLException sqle) {
-            sqle.printStackTrace();
-        }
-        return return_int;
-    }
-    
+  
    public int getMaxGrID() //OK    comprator = 8;
     {
         comprator = 9;
@@ -253,62 +104,6 @@ public class casaDB  extends dbObject {
             sqle.printStackTrace();
         }
         return return_int;
-    }
-     
-    public void setIDGr(int COD) //OK
-    {
-        this.id_groupe = COD;
-    }
-    public int getIDGr() //OK
-    {
-        return id_groupe;
-    }
-    public void setName(String Name) //OK
-    {
-        this.name = Name;
-    }
-    public String getName() //OK
-    {
-        return name;
-    }
-    public void setcode(int anid) //OK
-    {
-        this.code = anid;
-    }
-    public int getcode() //OK
-    {
-        return code;
-    }
-    public int getComprator() //OK
-    {
-        return comprator;
-    }
-    public void setComprator(int com) //OK
-    {
-        this.comprator = com;
-    }
-    
-    public void setComment(String Comment){
-    
-    this.comment = Comment;
-    
-    }
-    public String getComment(){
-    return comment;
-    }
-    
-    public void close() //OK
-    {
-        try{
-            rs.close();
-            rs=null;
-        }catch(java.sql.SQLException sqle){}
-        
-        try{
-            cstm.close();
-            cstm=null;
-        }catch(java.sql.SQLException sqle){}
-        
     }
     public String[] getCasaG() //test comprator = 6;
     {
