119f4c2fe5065170b5c84785f4df783e351d5c4f
johnniepop <johnniepop>
2006-03-04 09:43:01 +0000

    fix

--- src/imakante/install/sql/procedure/nom_procedure_doctype_user_rights.sql
+++ src/imakante/install/sql/procedure/nom_procedure_doctype_user_rights.sql
@@ -4,31 +4,31 @@ DROP PROCEDURE IF EXISTS `mida`.`nom_procedure__doc_type_user_rights` $$
 CREATE PROCEDURE `nom_procedure_doctype_user_rights`(IN comprator TINYINT, IN in_id INT(11), IN in_id_um INT(11), IN in_id_sdtn INT(11), IN in_rights INT(11))
 BEGIN
      IF (comprator = 0) THEN
-          SELECT n.id_ndtur, n.id_um, um.name_um, n.id_sdtn, dtn.area_number_sdtn, dtn.name_sdtn, n.rights_sdtn FROM n_doc_type_user_rights n
+          SELECT n.id_ndtur, n.id_um, um.name_um, n.id_sdtn, dtn.area_number_sdtn, dtn.name_sdtn, n.rights_ndtur FROM n_doc_type_user_rights n
             LEFT OUTER JOIN user_master um ON um.id_um = n.id_um
             LEFT OUTER JOIN sl_doc_type_num dtn ON dtn.id_sdtn = n.id_sdtn;
      END IF;
      IF (comprator = 1) THEN
-        INSERT INTO `mida`.`n_doc_type_user_rights`(id_um, id_sdtn, rights_sdtn) VALUES (in_id_um, in_id_sdtn, in_rights);
+        INSERT INTO `mida`.`n_doc_type_user_rights`(id_um, id_sdtn, rights_ndtur) VALUES (in_id_um, in_id_sdtn, in_rights);
      END IF;
      IF (comprator = 2) THEN
-        UPDATE `mida`.`n_doc_type_user_rights` SET id_um = in_id_um, id_sdtn = in_id_sdtn, rights_sdtn = in_rights
+        UPDATE `mida`.`n_doc_type_user_rights` SET id_um = in_id_um, id_sdtn = in_id_sdtn, rights_ndtur = in_rights
         WHERE `mida`.`n_doc_type_user_rights`.id_ndtur = in_id;
      END IF;
      IF (comprator = 3) THEN
         DELETE FROM `mida`.`n_doc_type_user_rights`  WHERE id_ndtur = in_id;
      END IF;
      IF (comprator = 4) THEN
-        SELECT n.id_ndtur, n.id_um, um.name_um, n.id_sdtn, dtn.area_number_sdtn, dtn.name_sdtn, n.rights_sdtn FROM n_doc_type_user_rights n
+        SELECT n.id_ndtur, n.id_um, um.name_um, n.id_sdtn, dtn.area_number_sdtn, dtn.name_sdtn, n.rights_ndtur FROM n_doc_type_user_rights n
             LEFT OUTER JOIN user_master um ON um.id_um = n.id_um
             LEFT OUTER JOIN sl_doc_type_num dtn ON dtn.id_sdtn = n.id_sdtn
             WHERE n.id_ndtur = in_id;
      END IF;
      IF (comprator = 5) THEN
-        SELECT n.id_ndtur, n.id_um, um.name_um, n.id_sdtn, dtn.area_number_sdtn, dtn.name_sdtn, n.rights_sdtn FROM n_doc_type_user_rights n
+        SELECT n.id_ndtur, n.id_um, um.name_um, n.id_sdtn, dtn.area_number_sdtn, dtn.name_sdtn, n.rights_ndtur FROM n_doc_type_user_rights n
             LEFT OUTER JOIN user_master um ON um.id_um = n.id_um
             LEFT OUTER JOIN sl_doc_type_num dtn ON dtn.id_sdtn = n.id_sdtn
-            WHERE n.rights_sdtn LIKE CONCAT('%',in_rights,'%');
+            WHERE n.rights_ndtur LIKE CONCAT('%',in_rights,'%');
      END IF;
      IF (comprator = 6) THEN
         SELECT um.id_um, um.name_um FROM user_master um;
