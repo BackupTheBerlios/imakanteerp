815bda5b938bdb9533503f43898fe25e346cd23b
imakante <imakante>
2006-03-22 15:07:44 +0000

    no message

--- src/imakante/com/priceToString.java
+++ src/imakante/com/priceToString.java
@@ -5,6 +5,8 @@ public class priceToString {
     
     public priceToString(double Value) {
         value = Value;
+        ConstString();
+        
     }
     private double value;
     String[] primary = {"един", "два", "три", "четири", "пет", "шест", "седем", "осем", "девет",
@@ -31,67 +33,96 @@ public class priceToString {
         
         if (valString.length()==9){
             int s = Integer.parseInt(valString.substring(0,1));
-            endString = endString + centum[s-1];
+            setEndString(getEndString() + centum[s-1]);
             valString = valString.substring(1,valString.length());
         }
         
         if (valString.length()==8){
             int s = Integer.parseInt(valString.substring(0,2));
             if (s<11){
-                if(!endString.equals("")){
-                    endString = endString + " и ";
+                if(!getEndString().equals("")){
+                    setEndString(getEndString() + " и ");
                 }
-                endString = endString +  primary[s-1];
+                setEndString(getEndString() + primary[s-1]);
                 valString = valString.substring(2,valString.length());
             }else{
-                endString = endString  + decade[s-1] + " и ";
+                setEndString(getEndString() + decade[s-1] + " и ");
                 valString = valString.substring(1,valString.length());}
         }
         if(valString.length()==7){
             
             int s = Integer.parseInt(valString.substring(0,1));
-            endString = endString + primary[s-1];
+            setEndString(getEndString() + primary[s-1]);
             valString = valString.substring(1,valString.length());
             if (s==1){
-                endString = endString + " милион ";
+                setEndString(getEndString() + " милион ");
             }else{
-                endString = endString + " милионa ";
+                setEndString(getEndString() + " милионa ");
             }
             
         }
         if (valString.length()==6){
             int s = Integer.parseInt(valString.substring(0,1));
-            endString = endString + centum[s-1];
+            setEndString(getEndString() + centum[s-1]);
             valString = valString.substring(1,valString.length());
         }
         if (valString.length()==5){
             int s = Integer.parseInt(valString.substring(0,2));
             if (s<11){
-                if(!endString.equals("")){
-                    endString = endString + " и ";
+                if(!getEndString().equals("")){
+                    setEndString(getEndString() + " и ");
                 }
-                endString = endString +  primary[s-1];
+                setEndString(getEndString() + primary[s-1]);
                 valString = valString.substring(2,valString.length());
             }else{
-                endString = endString  + decade[s-1] + " и ";
+                setEndString(getEndString() + decade[s-1] + " и ");
                 valString = valString.substring(1,valString.length());}
         }
-        if(valString.length()==5){
+        if(valString.length()==4){
             
             int s = Integer.parseInt(valString.substring(0,1));
             if (s==1){
-                if(endString.equals("")){
-                    endString = "хилияда";
+                if(getEndString().equals("")){
+                    setEndString("хилияда");
                 }else{
-                    endString = endString + " една хилияди ";}
+                    setEndString(getEndString() + " една хилияди ");}
+                
             }else{
-                endString = endString + primary[s-1];
-                valString = valString.substring(1,valString.length());}
-            
+                setEndString(getEndString() + primary[s-1] + " хиляди");
+            }
+            valString = valString.substring(1,valString.length());
             
         }
-        
-        
-        
+        if (valString.length()==3){
+            int s = Integer.parseInt(valString.substring(0,1));
+            setEndString(getEndString() + centum[s-1]);
+            valString = valString.substring(1,valString.length());
+        }
+        if (valString.length()==2){
+            int s = Integer.parseInt(valString.substring(0,2));
+            if (s<11){
+                if(!getEndString().equals("")){
+                    setEndString(getEndString() + " и ");
+                }
+                setEndString(getEndString() + primary[s-1]);
+                valString = valString.substring(2,valString.length());
+            }else{
+                setEndString(getEndString() + decade[s-1] + " и ");
+                valString = valString.substring(1,valString.length());}
+        }
+        if(valString.length()==1){
+            int s = Integer.parseInt(valString.substring(0,1));
+            setEndString(getEndString() + primary[s-1]);
+            
+        }
     }
+
+    public String getEndString() {
+        return endString;
+    }
+
+    public void setEndString(String endString) {
+        this.endString = endString;
+    }
+    
 }
