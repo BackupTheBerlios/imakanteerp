0fd9d27cefeafe1e877bdb80edd7ea56000e120d
imakante <imakante>
2006-03-29 18:03:39 +0000

    no message

--- src/imakante/sales/FrmCaseOperation.java
+++ src/imakante/sales/FrmCaseOperation.java
@@ -570,7 +570,7 @@ public class FrmCaseOperation extends  imakante.com.vcomponents.iInternalFrame i
         try {
             rs = stm.executeQuery("SELECT area_number_sdtn FROM sl_doc_type_num WHERE id_sdtn = " + sdtn);
             while(rs.next()){
-                sdtn = rs.getInt("area_number_sdtn");
+                sdtn = rs.getInt("id_sdtn ");
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
