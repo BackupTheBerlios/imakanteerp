004828842b7f317465db2cbc1eae22d2836270bc
imakante <imakante>
2006-05-06 08:10:40 +0000

    no message

---
+++ src/imakante/install/sql/ks1.sql
@@ -0,0 +1,8 @@
+SELECT  DISTINCT CONCAT(rep_comm_nal.id_pm,rep_comm_nal.code_n_storage,rep_comm_nal.code_contragent) AS con,  
+rep_comm_nal.code_n_storage,
+rep_comm_nal.name_pm,
+SUM(rep_comm_nal.quant_nal), SUM(rep_comm_nal.TSENA0)
+
+FROM rep_comm_nal
+WHERE rep_comm_nal.code_n_storage = 1
+GROUP BY con
