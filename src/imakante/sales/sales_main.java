7d2705fe2ec973435c65ec83385efe86bdc6280f
imakante <imakante>
2006-05-19 17:23:31 +0000

    no message

--- src/imakante/sales/sales_main.java
+++ src/imakante/sales/sales_main.java
@@ -136,6 +136,7 @@ public class sales_main extends imakante.com.vcomponents.iFrame {
         jMenuItem15 = new javax.swing.JMenuItem();
         jMenuItem10 = new javax.swing.JMenuItem();
         jMenuItem14 = new javax.swing.JMenuItem();
+        jMenuItem34 = new javax.swing.JMenuItem();
         balansMenu = new javax.swing.JMenu();
         balansMenu_stock = new javax.swing.JMenuItem();
         balansMenu_kol = new javax.swing.JMenuItem();
@@ -693,6 +694,9 @@ public class sales_main extends imakante.com.vcomponents.iFrame {
         jMenuItem14.setText("\u041f\u0440\u043e\u0434\u0430\u0436\u0431\u0438");
         sprMenu.add(jMenuItem14);
 
+        jMenuItem34.setText("\u041f\u0440\u043e\u0434\u0443\u043a\u0442\u043e\u0432\u0430 \u0440\u0435\u043d\u0442\u0430\u0431\u0438\u043b\u043d\u043e\u0441\u0442");
+        sprMenu.add(jMenuItem34);
+
         balansMenu.setText("\u0411\u0430\u043b\u0430\u043d\u0441\u0438");
         balansMenu_stock.setText("\u0421\u0442\u043e\u043a\u043e\u0432");
         balansMenu.add(balansMenu_stock);
@@ -1233,6 +1237,7 @@ public class sales_main extends imakante.com.vcomponents.iFrame {
     private javax.swing.JMenuItem jMenuItem31;
     private javax.swing.JMenuItem jMenuItem32;
     private javax.swing.JMenuItem jMenuItem33;
+    private javax.swing.JMenuItem jMenuItem34;
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem6;
