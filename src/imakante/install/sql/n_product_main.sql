11a129898fe0e9fc17da07136707e65407f71cbc
imakante <imakante>
2006-06-04 12:48:49 +0000

    no message

---
+++ src/imakante/install/sql/n_product_main.sql
@@ -0,0 +1,58 @@
+# MySQL-Front 3.2  (Build 13.39)
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='SYSTEM' */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+
+
+# Host: localhost    Database: mida
+# ------------------------------------------------------
+# Server version 5.0.22-community-nt
+
+USE `mida`;
+
+/*!40101 SET NAMES cp1251 */;
+
+#
+# Table structure for table n_product_main
+#
+
+DROP TABLE IF EXISTS `n_product_main`;
+CREATE TABLE `n_product_main` (
+  `id_pm` int(11) unsigned NOT NULL auto_increment,
+  `id_pd` int(11) unsigned NOT NULL default '0',
+  `id_n_group` int(11) unsigned NOT NULL default '0',
+  `id_ppp` int(11) unsigned NOT NULL default '0',
+  `id_pp` int(11) unsigned NOT NULL default '0',
+  `id_pf` int(11) unsigned NOT NULL default '0',
+  `name_pm` varchar(50) default NULL,
+  `fname_pm` varchar(50) default NULL,
+  `sname_pm` varchar(50) default NULL,
+  `cname_pm` varchar(50) default NULL,
+  `cod1_pm` varchar(20) default NULL,
+  `cod2_pm` varchar(20) default NULL,
+  `barcod_pm` int(11) unsigned default NULL,
+  `max_pop_pm` double default NULL,
+  `expertsheet_pm` varchar(45) default NULL,
+  `flag_pm` int(3) unsigned zerofill NOT NULL default '000',
+  `min_pm` int(11) unsigned NOT NULL default '0',
+  `code_pm` varchar(11) NOT NULL default '0',
+  PRIMARY KEY  (`id_pm`)
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/nom/showContentsCon_Gr.java
+++ src/nom/showContentsCon_Gr.java
@@ -5,12 +5,10 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.*;
 
-public class showContentsCon_Gr   extends imakante.com.vcomponents.iDialog
-{
+public class showContentsCon_Gr   extends imakante.com.vcomponents.iDialog {
     
     /** Creates new form aContragent */
-    public showContentsCon_Gr(imakante.com.vcomponents.iInternalFrame myframe, boolean modal, java.sql.ResultSet rs, java.sql.Connection con)
-    {
+    public showContentsCon_Gr(imakante.com.vcomponents.iInternalFrame myframe, boolean modal, java.sql.ResultSet rs, java.sql.Connection con) {
         super(myframe, modal);
         myParent = (nom.FrmProduct)  myframe;
         this.rs = rs;
@@ -18,28 +16,23 @@ public class showContentsCon_Gr   extends imakante.com.vcomponents.iDialog
         //columnName = null;
         model = new imakante.com.CustomTableModel(conn,rs, columnName);
         table = new imakante.com.CustomTable(model);
-       HideColumns(getColumnIndex("id_contragent"));
-       HideColumns(getColumnIndex("is_oso"));
-       HideColumns(getColumnIndex("id_mol"));
-       HideColumns(getColumnIndex("id_nm"));
-       HideColumns(getColumnIndex("flag_n_contragent"));
-        table.addKeyListener(new KeyListener()
-        {
-            public void keyPressed(KeyEvent e)
-            {
-              if(java.awt.event.KeyEvent.VK_ENTER == e.getKeyCode())
-                {
-                 int row = table.getSelectedRow();
-                 int ID = (Integer)table.getValueAt(row,getColumnIndex("id_contragent"));
-                 myParent.setId_Contragent(ID);
-                 close();
+        HideColumns(getColumnIndex("id_contragent"));
+        HideColumns(getColumnIndex("is_oso"));
+        HideColumns(getColumnIndex("id_mol"));
+        HideColumns(getColumnIndex("id_nm"));
+        HideColumns(getColumnIndex("flag_n_contragent"));
+        table.addKeyListener(new KeyListener() {
+            public void keyPressed(KeyEvent e) {
+                if(java.awt.event.KeyEvent.VK_ENTER == e.getKeyCode()) {
+                    int row = table.getSelectedRow();
+                    int ID = (Integer)table.getValueAt(row,getColumnIndex("id_contragent"));
+                    myParent.setId_Contragent(ID);
+                    close();
                 }
             }
-            public void keyReleased(KeyEvent e)
-            {
+            public void keyReleased(KeyEvent e) {
             }
-            public void keyTyped(KeyEvent e)
-            {
+            public void keyTyped(KeyEvent e) {
             }
         });
         
@@ -50,7 +43,10 @@ public class showContentsCon_Gr   extends imakante.com.vcomponents.iDialog
         int x = (((dim.width)-(this.getSize().width))/2);
         int y = (((dim.height)-(this.getSize().height))/2);
         this.setLocation(x, y);
-      //  setVisible(true);
+        this.table.requestFocus();
+        this.table.changeSelection(0,0,false,false);
+        
+        //  setVisible(true);
         
     }
     
@@ -64,6 +60,7 @@ public class showContentsCon_Gr   extends imakante.com.vcomponents.iDialog
         jScrollPane1 = new javax.swing.JScrollPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("\u0418\u0417\u0411\u041e\u0420");
         jScrollPane1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jScrollPane1.setOpaque(false);
         jScrollPane1.setPreferredSize(new java.awt.Dimension(200, 200));
@@ -78,58 +75,54 @@ public class showContentsCon_Gr   extends imakante.com.vcomponents.iDialog
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
+    
     private void jScrollPane1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jScrollPane1KeyPressed
 // TODO add your handling code here:
-        if(java.awt.event.KeyEvent.VK_ENTER == evt.getKeyCode())
-        {
+        if(java.awt.event.KeyEvent.VK_ENTER == evt.getKeyCode()) {
             int row = table.getSelectedRow();
             int ID = (Integer)table.getValueAt(row,0);
-           // myParent.
-            this.dispose();  
+            // myParent.
+            this.dispose();
         }
-            
+        
         
         
     }//GEN-LAST:event_jScrollPane1KeyPressed
     
-   
+    
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
     private  imakante.com.CustomTableModel model;
-    private  imakante.com.CustomTable table; 
+    private  imakante.com.CustomTable table;
     private  java.sql.ResultSet rs;
     private nom.FrmProduct myParent;
     private java.sql.Connection conn;
     private int Con_Gr =0;
   /*  private String columnName[] = {"id_contragent","is_oso","id_mol","id_nm","Код:","Име:","Булстат:","Данъчен номер:",
                                   "Адрес:", "Телефон:","Факс:","E-mail:","Web page","flag_n_contragent"};*/
-     private String columnName[] = {"id_contragent","is_oso","id_mol","id_nm","\u041a\u043e\u0434:","\u0418\u043c\u0435:","\u0411\u0443\u043b\u0441\u0442\u0430\u0442:","\u0414\u0430\u043d\u044a\u0447\u0435\u043d \u043d\u043e\u043c\u0435\u0440:",
-"\u0410\u0434\u0440\u0435\u0441:","\u0422\u0435\u043b\u0435\u0444\u043e\u043d:","\u0424\u0430\u043a\u0441:","E-mail:","Web page","flag_n_contragent"};
+    private String columnName[] = {"id_contragent","is_oso","id_mol","id_nm","\u041a\u043e\u0434:","\u0418\u043c\u0435:","\u0411\u0443\u043b\u0441\u0442\u0430\u0442:","\u0414\u0430\u043d\u044a\u0447\u0435\u043d \u043d\u043e\u043c\u0435\u0440:",
+    "\u0410\u0434\u0440\u0435\u0441:","\u0422\u0435\u043b\u0435\u0444\u043e\u043d:","\u0424\u0430\u043a\u0441:","E-mail:","Web page","flag_n_contragent"};
     
-    public void close()
-    {
+    public void close() {
         this.dispose();
     }
-private int getColumnIndex(String in) //test
- {
-     int count = table.getColumnCount();
-     for(int i=0; i < count; i++)
-     {
-         if(table.getColumnName(i).equals(in)) return i;
-     }
-     return 0;
- }
- private void HideColumns(int col)
- {
-   int iColumn = col;
+    private int getColumnIndex(String in) //test
+    {
+        int count = table.getColumnCount();
+        for(int i=0; i < count; i++) {
+            if(table.getColumnName(i).equals(in)) return i;
+        }
+        return 0;
+    }
+    private void HideColumns(int col) {
+        int iColumn = col;
 // set column width
-table.getColumnModel().getColumn(iColumn).setMaxWidth(0);
-table.getColumnModel().getColumn(iColumn).setMinWidth(0);
-table.getTableHeader().getColumnModel().getColumn(iColumn).setMaxWidth(0);
-table.getTableHeader().getColumnModel().getColumn(iColumn).setMinWidth(0);
-     
- }
+        table.getColumnModel().getColumn(iColumn).setMaxWidth(0);
+        table.getColumnModel().getColumn(iColumn).setMinWidth(0);
+        table.getTableHeader().getColumnModel().getColumn(iColumn).setMaxWidth(0);
+        table.getTableHeader().getColumnModel().getColumn(iColumn).setMinWidth(0);
+        
+    }
 }
