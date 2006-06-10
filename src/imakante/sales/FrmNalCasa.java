4588a499244f172fc9bbf5a6272fce89bbdbe36e
johnniepop <johnniepop>
2006-06-10 13:59:06 +0000

    test ready

--- src/imakante/sales/FrmNalCasa.java
+++ src/imakante/sales/FrmNalCasa.java
@@ -1,11 +1,6 @@
 
 package imakante.sales;
 
-import java.awt.event.WindowEvent;
-import java.net.MalformedURLException;
-import java.sql.SQLException;
-
-
 public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implements java.awt.event.WindowListener {
     
     public FrmNalCasa(String title,imakante.com.vcomponents.iFrame frame, int levelx) {
@@ -15,7 +10,6 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
         prepareStm();
         initComponents();
     }
-    
     
     // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -30,12 +24,13 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
 
         setClosable(true);
         setIconifiable(true);
-        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
-        jLabel1.setText("\u041a\u0430\u0441\u0430        \u043e\u0442:");
+        setFrameIcon(new javax.swing.ImageIcon(getClass().getResource("/images/imakante_ico.png")));
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEtchedBorder(javax.swing.border.EtchedBorder.RAISED), "\u041a\u0410\u0421\u0410", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 1, 11)));
+        jLabel1.setText("\u043e\u0442:");
 
-        jTextField1.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
-            public void propertyChange(java.beans.PropertyChangeEvent evt) {
-                jTextField1PropertyChange(evt);
+        jTextField1.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                jTextField1FocusGained(evt);
             }
         });
         jTextField1.addKeyListener(new java.awt.event.KeyAdapter() {
@@ -47,6 +42,9 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
         jLabel2.setText("\u0434\u043e:");
 
         jTextField2.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                jTextField2FocusGained(evt);
+            }
             public void focusLost(java.awt.event.FocusEvent evt) {
                 jTextField2FocusLost(evt);
             }
@@ -62,22 +60,25 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
                 .add(jLabel1)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(18, 18, 18)
                 .add(jTextField1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 139, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(jLabel2)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(jTextField2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 152, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(24, Short.MAX_VALUE))
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                .add(jLabel1)
-                .add(jTextField1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .add(jLabel2)
-                .add(jTextField2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+            .add(jPanel1Layout.createSequentialGroup()
+                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(jTextField1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(jLabel1)
+                    .add(jLabel2)
+                    .add(jTextField2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jButton1.setText("\u0413\u0435\u043d\u0435\u0440\u0438\u0440\u0430\u0439");
@@ -99,7 +100,7 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(org.jdesktop.layout.GroupLayout.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addContainerGap(253, Short.MAX_VALUE)
+                .addContainerGap(207, Short.MAX_VALUE)
                 .add(jButton1)
                 .add(11, 11, 11)
                 .add(jButton2)
@@ -118,25 +119,33 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(layout.createSequentialGroup()
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(jPanel1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .add(org.jdesktop.layout.GroupLayout.TRAILING, jPanel3, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, jPanel1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, jPanel3, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(layout.createSequentialGroup()
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .add(jPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .add(jPanel1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(jPanel3, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .add(jPanel3, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-        setBounds((screenSize.width-454)/2, (screenSize.height-133)/2, 454, 133);
+        setBounds((screenSize.width-408)/2, (screenSize.height-146)/2, 408, 146);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField2FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField2FocusGained
+        jTextField2.selectAll();
+    }//GEN-LAST:event_jTextField2FocusGained
+
+    private void jTextField1FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField1FocusGained
+        jTextField1.selectAll();
+    }//GEN-LAST:event_jTextField1FocusGained
     
     private void jTextField2FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField2FocusLost
         checkValue();
@@ -150,30 +159,23 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
     private void jTextField2KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField2KeyPressed
         if(java.awt.event.KeyEvent.VK_F7== evt.getKeyCode()){
             processField2();
-        }
-        if(java.awt.event.KeyEvent.VK_ENTER == evt.getKeyCode()){
+        } else if(java.awt.event.KeyEvent.VK_ENTER == evt.getKeyCode()){
             if(jTextField2.getText().equals("")){jTextField2.setText("999999999");jTextField2.transferFocus();}
-        }
+        } else if (evt.getKeyCode() == java.awt.event.KeyEvent.VK_ESCAPE) { jTextField2.setText(""); }
     }//GEN-LAST:event_jTextField2KeyPressed
     
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
-    
-    private void jTextField1PropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_jTextField1PropertyChange
-// TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1PropertyChange
-    
+        
     private void jTextField1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField1KeyPressed
         if(java.awt.event.KeyEvent.VK_F7== evt.getKeyCode()){
             processField1();
-        }
-        if(java.awt.event.KeyEvent.VK_ENTER == evt.getKeyCode()){
-            if(jTextField1.getText().equals("")){jTextField1.setText("1");jTextField1.transferFocus();}else{jTextField1.transferFocus();}
-            
-        }
+        } else if(java.awt.event.KeyEvent.VK_ENTER == evt.getKeyCode()) {
+            if(jTextField1.getText().equals("")){jTextField1.setText("1");jTextField1.transferFocus();
+            }else{jTextField1.transferFocus();}
+        } else if (evt.getKeyCode() == java.awt.event.KeyEvent.VK_ESCAPE) { jTextField1.setText(""); }
     }//GEN-LAST:event_jTextField1KeyPressed
-    
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
@@ -186,18 +188,12 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
     private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
     private  imakante.com.vcomponents.iFrame myframe;
-    
     private  java.sql.Connection conn;
     private java.sql.Statement stm;
     private  java.sql.ResultSet rs;
-    
-    private java.io.FileInputStream fs=null;
-    private java.net.URL url=null;
-    
-    private java.util.HashMap hm = null;
-    
     private  imakante.com.CustomTableModel model;
     private  imakante.com.CustomTable table;
+    private java.util.HashMap hm = null;
     private String idFCodeContr = "0";
     private String  idLastContr = "99999999999";
     private String[] Names= { "\u041a\u041e\u0414", "\u0418\u041c\u0415"};
@@ -208,14 +204,11 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
             + "`n_casa`.`code_n_casa`, `n_casa`.`name_n_casa`"
             + "FROM `n_casa` WHERE `n_casa`.`code_n_casa` LIKE  '%";
     
-    
-    
     private String[] NamesQ= {"\u043a\u0430\u0441\u0438", "\u043f\u0440\u0438\u0445\u043e\u0434", "\u0440\u0430\u0437\u0445\u043e\u0434","\u043d\u0430\u043b\u0438\u0447\u043d\u0438"};
     private String qu =  "";
     private int levelx = 1;
     private String casaBegin = "0";
     private String casaEnd = "999999999";
-    
     
     //METHODS
     private void checkValue(){
@@ -238,6 +231,7 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
             jTextField2.setText("999999999");
         }
     }
+    
     private void constString(){
         qu =    " SELECT distinct code_n_casa AS casa, "
                 + " IFNULL(@prihod :=(SELECT DISTINCT rep_casa_nal.suma FROM rep_casa_nal WHERE code_n_casa = casa AND rep_casa_nal.in_type_sl_mop = 1 AND rep_casa_nal.levelx = " + levelx +" GROUP BY casa), 0) AS prihod, "
@@ -250,7 +244,7 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
     
     private void constHash(){
         hm = new java.util.HashMap();
-        hm.put("casaBegin",this.jTextField1.getText());
+        hm.put("casaBegin", this.jTextField1.getText());
         hm.put("casaEnd", this.jTextField2.getText());
         hm.put("levelx", "" + levelx);
     }
@@ -258,29 +252,23 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
     private void prepareConn() {
         try{
             conn =  myframe.getConn();
-            if(conn==null){System.out.println("conn problem");
-            }
-        }catch(Exception e){e.printStackTrace();}
+            if(conn==null) System.out.println("conn problem");
+        } catch(Exception e) { e.printStackTrace(); }
     }
     
     private void prepareStm(){
         try {
             stm = conn.createStatement();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+        } catch (java.sql.SQLException ex) { ex.printStackTrace(); }
     }
     
     private void initTable(String str , String[] names) {
         try {
             rs = stm.executeQuery(str);
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+        } catch (java.sql.SQLException ex) { ex.printStackTrace(); }
         try {
             model = new imakante.com.CustomTableModel(getConn(), rs, NamesQ);
             table = new imakante.com.CustomTable(model);
-            
         } catch(Exception e) { e.printStackTrace(); }
         
         try {
@@ -288,7 +276,7 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
         } catch(Exception ex) {  }
     }
     
-    private void constructDialod(String str, int rCompNumber, String[] names){
+    private void constructDialod(String str, int rCompNumber, String[] names) {
         this.CompNumber = rCompNumber;
         initTable(str, names );
         imakante.com.vcomponents.tableDialog td = new imakante.com.vcomponents.tableDialog(this, true, table, "\u0418\u0437\u0431\u043e\u0440", "");
@@ -296,19 +284,13 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
         
     }
     
-    
-    
-    
-    private int getColumnIndex(String in) //test
-    {
+    private int getColumnIndex(String in) {
         int count = table.getColumnCount();
-        for(int i=0; i < count; i++) {
+        for(int i = 0; i < count; i++) {
             if(table.getColumnName(i).equals(in)) return i;
         }
         return 0;
     }
-    
-    
     
     public java.sql.Connection getConn() {
         return conn;
@@ -318,25 +300,19 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
         this.conn = conn;
     }
     
-    public void windowOpened(WindowEvent e) {
+    public void windowOpened(java.awt.event.WindowEvent e) {
     }
-    
-    public void windowClosing(WindowEvent e) {
+    public void windowClosing(java.awt.event.WindowEvent e) {
     }
-    
-    public void windowClosed(WindowEvent e) {
+    public void windowClosed(java.awt.event.WindowEvent e) {
     }
-    
-    public void windowIconified(WindowEvent e) {
+    public void windowIconified(java.awt.event.WindowEvent e) {
     }
-    
-    public void windowDeiconified(WindowEvent e) {
+    public void windowDeiconified(java.awt.event.WindowEvent e) {
     }
-    
-    public void windowActivated(WindowEvent e) {
+    public void windowActivated(java.awt.event.WindowEvent e) {
     }
-    
-    public void windowDeactivated(WindowEvent e) {
+    public void windowDeactivated(java.awt.event.WindowEvent e) {
     }
     
     public String[] getNames() {
@@ -348,34 +324,29 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
     }
     
     public int getIntTransfer() {
-        
         return intTransfer;
     }
     
     public void setIntTransfer(int intTransfer) {
         this.intTransfer = intTransfer;
-        if (CompNumber == 0){}
-        if(CompNumber == 1){
-            this.jTextField1.setText(""+this.intTransfer);}
-        if(CompNumber == 2){
-            this.jTextField2.setText(""+this.intTransfer);}
-        
+        if (CompNumber == 0) {}
+        if (CompNumber == 1) this.jTextField1.setText("" + this.intTransfer);
+        if (CompNumber == 2) this.jTextField2.setText("" + this.intTransfer);
     }
     
     private void processField1() {
         String newString = strCasa + this.jTextField1.getText() + "%'";
         constructDialod(newString, 1, Names);
-        
     }
     
     private void processField2() {
-        if(this.jTextField1.getText()==""){this.jTextField1.setText("0");}
-        String newString = strCasa + this.jTextField2.getText() + "%' AND `n_casa`.`code_n_casa` >= " + this.jTextField1.getText() + ";";
+        if(this.jTextField1.getText() == "") this.jTextField1.setText("0");
+        String newString = 
+                strCasa + this.jTextField2.getText() + "%' AND `n_casa`.`code_n_casa` >= " + this.jTextField1.getText() + ";";
         constructDialod(newString, 2, Names);
     }
     
-    
-    private void processReport(){
+    private void processReport() {
         checkValue();
         casaBegin = this.jTextField1.getText();
         casaEnd = this.jTextField2.getText();
@@ -384,20 +355,18 @@ public class FrmNalCasa extends imakante.com.vcomponents.iInternalFrame implemen
         try{
             initTable(qu, NamesQ);
             imakante.com.vcomponents.tableDialog td = new imakante.com.vcomponents.tableDialog(this, true, table,
-                    myframe.getConn(), hm, "/imakante/sales/jasper/nal_casi_simpl.jasper", "\u041a\u0430\u0441\u043e\u0432\u0438 \u041d\u0430\u043b\u0438\u0447\u043d\u043e\u0441\u0442\u0438", "");
+                    myframe.getConn(), hm, "nal_casi_simpl.jasper", "\u041a\u0430\u0441\u043e\u0432\u0438 \u041d\u0430\u043b\u0438\u0447\u043d\u043e\u0441\u0442\u0438", "");
             td.setVisible(true);
-        } catch (Exception  ex){
+        } catch (Exception  ex) {
             ex.printStackTrace();
             constructDialod(qu, 0, NamesQ);
             System.out.println("Ne moga da nameria faila  ");
         }
-        
     }
     
     private void fillBlanck(){
-        if(jTextField1.getText()==""){jTextField1.setText("1");}
-        if(jTextField2.getText()==""){jTextField2.setText("999999999");}
-        
+        if(jTextField1.getText() == "") jTextField1.setText("1");
+        if(jTextField2.getText() == "") jTextField2.setText("999999999");
     }
     
 }
