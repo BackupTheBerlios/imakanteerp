a4bdc840597bb173ec67cee3fba023fa5999908d
johnniepop <johnniepop>
2006-10-06 13:27:53 +0000

    no message

--- src/imakante/install/sql/rep_contra_debts_and_payments.sql
+++ src/imakante/install/sql/rep_contra_debts_and_payments.sql
@@ -1,10 +1,10 @@
 # This VIEW represent the complete list of contragent(-clients) debts extracted from the sl_document_facade table
 
-CREATE VIEW rep_contra_debts AS
+CREATE VIEW rep_contra_debts_and_payments AS
 SELECT d.id_df AS 'id', 
     d.out_contragent_df AS 'refNC', 
     nc.code_contragent AS 'codeContragent', 
-    nc.name_n_contragent AS 'Kontragent'
+    nc.name_n_contragent AS 'Kontragent', 
     d.total_df AS 'suma', 
     d.date_edition_df AS 'ot data', 
     d.description_pay_df AS 'refKP', 
@@ -13,9 +13,9 @@ SELECT d.id_df AS 'id',
     d.date_pay_df AS 'Pay Day', 
     
     d.level_df AS 'LEVEL'
-    FROM sl_document_facade_df d 
+    FROM sl_document_facade d 
     LEFT JOIN sl_m_operation mop ON mop.id_sl_mop = d.paying_order_df 
     
     LEFT JOIN n_contragent nc ON nc.id_contragent = d.out_contragent_df 
     LEFT JOIN kind_paying kp ON kp.id_kp = d.description_pay_df 
-    WHERE (d.description_pay_df = 1 OR d.description_pay_df = 4);
+    WHERE d.out_contragent_df > 0;
