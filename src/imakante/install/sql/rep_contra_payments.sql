64c2e499470d8118aa8c63e51b5c8d40c2a6ac22
johnniepop <johnniepop>
2006-10-10 15:49:56 +0000

    no message

--- src/imakante/install/sql/rep_contra_payments.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIEW rep_contra_payments AS
-SELECT id_df, out_contragent_df, total_df, description_pay_df, date_pay_df
-    FROM sl_document_facade df
-    WHERE paying_order_df IS NOT NULL;
