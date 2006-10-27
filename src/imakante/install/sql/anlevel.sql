c0cf40cef4b520c455a2143785eddc708b1fda82
johnniepop <johnniepop>
2006-10-27 08:16:36 +0000

    structure changes

---
+++ src/imakante/install/sql/anlevel.sql
@@ -0,0 +1,40 @@
+# MySQL-Front 3.2  (Build 10.2)
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+
+/*!40101 SET NAMES cp1251 */;
+/*!40103 SET TIME_ZONE='SYSTEM' */;
+
+# Host: localhost:3307    Database: mida
+# ------------------------------------------------------
+# Server version 5.0.16-nt
+
+#
+# Table structure for table anlevel
+#
+
+DROP TABLE IF EXISTS `anlevel`;
+CREATE TABLE `anlevel` (
+  `id_al` int(10) unsigned NOT NULL auto_increment,
+  `cod_al` varchar(45) NOT NULL,
+  `name_al` varchar(45) NOT NULL,
+  PRIMARY KEY  (`id_al`)
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+#
+# Dumping data for table anlevel
+#
+
+INSERT INTO `anlevel` VALUES (1,'1','OB');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
