08ca8acd340eb046cc7e16568fb7e74fc4bd273f
imakante <imakante>
2006-11-02 14:49:13 +0000

    no message

---
+++ src/imakante/install/sql/view/rep_comm_nal1.sql
@@ -0,0 +1,10 @@
+INSERT INTO rep_comm_nal1
+  (id_pc, level, quant_nal, id_n_storage, code_n_storage, name_n_storage, id_pm, parcel_pc, dateofexpire_pc, barcod_pc, id_pp, id_ppp, id_pf, exp_list_pc, id_pd, id_n_group, name_pm, fname_pm, sname_pm, cname_pm, cod1_pm, cod2_pm, barcod_pm, max_pop_pm, expertsheet_pm, flag_pm, min_pm, code_pm, id_sl_curs, price1_pp, price2_pp, price3_pp, price0_pp)
+VALUES
+  (1, 0, 1, 1, 1, "ЦЕНТРАЛЕН", 3, 1, "1.11.2006", 0, 0, 0, 0, "", 1, 15, "БОЛЯРКА БОКС 4", "БОЛЯРКА БОКС 4", "БОЛЯРКА БО", "БОЛЯРКА БО", "", "", "2121211", 0, "12221", 0, 1, 1, 1, "0,12", "0,145", "0,152", "0,11");
+
+INSERT INTO rep_comm_nal1
+  (id_pc, level, quant_nal, id_n_storage, code_n_storage, name_n_storage, id_pm, parcel_pc, dateofexpire_pc, barcod_pc, id_pp, id_ppp, id_pf, exp_list_pc, id_pd, id_n_group, name_pm, fname_pm, sname_pm, cname_pm, cod1_pm, cod2_pm, barcod_pm, max_pop_pm, expertsheet_pm, flag_pm, min_pm, code_pm, id_sl_curs, price1_pp, price2_pp, price3_pp, price0_pp)
+VALUES
+  (1, 1, 1, 1, 1, "ЦЕНТРАЛЕН", 3, 1, "1.11.2006", 0, 0, 0, 0, "", 1, 15, "БОЛЯРКА БОКС 4", "БОЛЯРКА БОКС 4", "БОЛЯРКА БО", "БОЛЯРКА БО", "", "", "2121211", 0, "12221", 0, 1, 1, 1, "0,12", "0,145", "0,152", "0,11");
+
