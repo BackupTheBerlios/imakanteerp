851bd546c37b68f311a3c46784cf091c8165d2ef
johnniepop <johnniepop>
2007-02-09 19:30:19 +0000

    laf testing

--- src/imakante/salary/salary_main.java
+++ src/imakante/salary/salary_main.java
@@ -16,6 +16,8 @@ public class salary_main extends imakante.com.vcomponents.iFrame {
         jmiStaff = new javax.swing.JMenuItem();
         jsStaff2Exit = new javax.swing.JSeparator();
         jmiExit = new javax.swing.JMenuItem();
+        jmServices = new javax.swing.JMenu();
+        jmLF = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         jmOperations.setText("\u041e\u043f\u0435\u0440\u0430\u0446\u0438\u0438");
@@ -37,17 +39,24 @@ public class salary_main extends imakante.com.vcomponents.iFrame {
 
         jmbSalary.add(jmOperations);
 
+        jmServices.setText("\u0421\u041b\u0423\u0416\u0415\u0411\u041d\u0418");
+        new imakante.com.LFManager(this.jmLF);
+        jmLF.setText("\u0412\u044a\u043d\u0448\u0435\u043d \u0432\u0438\u0434 \u043d\u0430 \u041f\u0440\u043e\u0433\u0440\u0430\u043c\u0430\u0442\u0430");
+        jmServices.add(jmLF);
+
+        jmbSalary.add(jmServices);
+
         setJMenuBar(jmbSalary);
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(0, 400, Short.MAX_VALUE)
+            .add(0, 392, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(0, 279, Short.MAX_VALUE)
+            .add(0, 239, Short.MAX_VALUE)
         );
         java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
         setBounds((screenSize.width-400)/2, (screenSize.height-300)/2, 400, 300);
@@ -59,7 +68,9 @@ public class salary_main extends imakante.com.vcomponents.iFrame {
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jmLF;
     private javax.swing.JMenu jmOperations;
+    private javax.swing.JMenu jmServices;
     private javax.swing.JMenuBar jmbSalary;
     private javax.swing.JMenuItem jmiExit;
     private javax.swing.JMenuItem jmiStaff;
